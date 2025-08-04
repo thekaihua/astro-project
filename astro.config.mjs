e0af40d8a089abfbfe1409af06e36a93783ca82d
thekaihua <215784773+thekaihua@users.noreply.github.com>
2025-08-04 18:05:40 +0800

    feat: add Preact support and RSS feed generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), preact()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  
+  site: "https://jovial-gelato-b32983.netlify.app/"
 });
